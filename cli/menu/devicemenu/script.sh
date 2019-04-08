237b5b38484cf58afd7542801e07202c76e07b44
Don Newton <donNewtonAlpha@gmail.com>
2019-04-08 17:06:33 -0400

    vol-1552 - inital stub
    Change-Id: I9f5c93eefbde51c64a7c1b13244eca3a16de8a41

---
+++ cli/menu/devicemenu/script.sh
@@ -0,0 +1,4 @@
+for i in edit history img_dnld_request perf_config save eof images img_dnld_status ports set exit img_activate img_revert py shell flows img_dnld_cancel list quit shortcuts help img_dnld_list pause run show
+do
+   vi $i.go
+done
