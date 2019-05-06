c5a09968249d21eec4aec580fb4b7ae0d149ede3
Devmalya Paul <Devmalya.Paul@radisys.com>
2019-05-07 04:04:23 +0530

    VOL 1537 Alarm framework added for openolt adapter
    Change-Id: Ie078b92ded609d8b3b03b488757232b1fa55b7c4

---
+++ extentions/alarms/onu_alarms/onu_activation_fail_alarm.go
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2018-present Open Networking Foundation
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package onu_alarms
+
+import (
+	"fmt"
+	"github.com/opencord/voltha-go/common/log"
+	ab "github.com/opencord/voltha-go/extentions/alarms"
+	"github.com/opencord/voltha-go/kafka"
+	"github.com/opencord/voltha-protos/go/voltha"
+	"time"
+)
+
+type OnuActivationFailAlarm struct {
+	AlarmData    ab.AlarmData
+	AlarmContext map[string]string
+}
+
+func GetNewOnuActivationFailAlarm() OnuActivationFailAlarm {
+	var los OnuActivationFailAlarm
+	return los
+}
+
+func (onuAlarm OnuActivationFailAlarm) GetAlarmData(status bool, deviceId string) ab.AlarmData {
+	var alarmData ab.AlarmData
+	alarmData.Ts = float32(time.Now().UnixNano())
+	alarmData.Description = FormatDescription("ONU_ACTIVATION_FAILED_ALARM", true)
+	alarmData.Id = FormatId("ONU_ACTIVATION_FAILED_ALARM")
+	alarmData.Category = voltha.AlarmEventCategory_ONU
+	alarmData.Severity = voltha.AlarmEventSeverity_MAJOR
+	alarmData.Type = voltha.AlarmEventType_COMMUNICATION
+	alarmData.LogicalDeviceId = deviceId
+	if status {
+		alarmData.State = voltha.AlarmEventState_RAISED
+	} else {
+		alarmData.State = voltha.AlarmEventState_CLEARED
+	}
+	alarmData.Name = "ONU_ACTIVATION_FAILED_ALARM"
+
+	return alarmData
+}
+
+func (onuAlarm OnuActivationFailAlarm) GetContextData(onuId uint32, intfId string) map[string]string {
+	alarmContext := make(map[string]string)
+	alarmContext["onu-intf-id"] = intfId
+	alarmContext["onu-id"] = string(onuId)
+	return alarmContext
+}
+
+func (onuAlarm OnuActivationFailAlarm) FormatDescription(alarmName string, status bool) string {
+	if status {
+		return fmt.Sprintf("Alarm - %s - RAISED", alarmName)
+	}
+	return fmt.Sprintf("Alarm - %s - CLEARED", alarmName)
+}
+
+func (onuAlarm OnuActivationFailAlarm) FormatId(alarmName string) string {
+	return fmt.Sprintf("Voltha.openolt.%s.%s", alarmName, string(time.Now().Nanosecond()))
+}
+
+func (onuAlarm OnuActivationFailAlarm) RaiseAlarm(kc kafka.Client, topic kafka.Topic) error {
+	ae := ab.CreateAlarmEvent(&onuAlarm.AlarmData, onuAlarm.AlarmContext)
+	if err := ab.SendAlarm(kc, topic, ae); err != nil {
+		log.Errorw("Failed to send alarm to kafka", log.Fields{"alarm_event": ae, "topic": topic.Name, "error": err})
+		return err
+	}
+	log.Infow("Successfully sent alarm to kafka", log.Fields{"alarm_event": ae, "topic": topic.Name})
+	return nil
+}
+
+func (onuAlarm OnuActivationFailAlarm) ClearAlarm(kc kafka.Client, topic kafka.Topic) error {
+	ae := ab.CreateAlarmEvent(&onuAlarm.AlarmData, onuAlarm.AlarmContext)
+	if err := ab.SendAlarm(kc, topic, ae); err != nil {
+		log.Errorw("Failed to send alarm to kafka", log.Fields{"alarm_event": ae, "topic": topic.Name, "error": err})
+		return err
+	}
+	log.Infow("Successfully sent alarm to kafka", log.Fields{"alarm_event": ae, "topic": topic.Name})
+	return nil
+}
