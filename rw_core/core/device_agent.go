e0d07f9ca0e46d70b4acb90ad5edc3bceceb0079
onkarkundargi <onkar.kundargi@gmail.com>
2020-01-27 11:34:52 +0530

    SEBA-749 #comments Added on demand api code for omci test action
    Change-Id: I1a52dc5ec78ac61001e434c8c98f0400e034dc50

--- rw_core/core/device_agent.go
+++ rw_core/core/device_agent.go
@@ -1449,6 +1449,7 @@ func (agent *DeviceAgent) enablePort(ctx context.Context, Port *voltha.Port) err
 	return nil
 }
 
+
 func (agent *DeviceAgent) ChildDeviceLost(ctx context.Context, device *voltha.Device) error {
 
 	agent.lockDevice.Lock()
@@ -1465,5 +1466,26 @@ func (agent *DeviceAgent) ChildDeviceLost(ctx context.Context, device *voltha.De
 		log.Warnw("ChildDeviceLost-error", log.Fields{"error": err})
 	}
 	return nil
+}
 
+func (agent *DeviceAgent) startOmciTest(ctx context.Context, omcitestrequest *voltha.OmciTestRequest) (*voltha.TestResponse, error){
+	agent.lockDevice.Lock()
+	defer agent.lockDevice.Unlock()
+	if device, err := agent.getDeviceWithoutLock(); err != nil {
+		return &voltha.TestResponse{Result: voltha.TestResponse_FAILURE}, nil
+	} else {
+		if adapterName, err := agent.adapterMgr.getAdapterName(device.Type); err != nil {
+			log.Warnw("no-adapter-registered-for-device-type", log.Fields{"deviceType": device.Type, "deviceAdapter": device.Adapter})
+			return &voltha.TestResponse{Result: voltha.TestResponse_FAILURE}, nil
+		} else {
+			device.Adapter = adapterName
+		}
+		if resp, _ := agent.adapterProxy.startOmciTest(ctx, device, omcitestrequest); err != nil {
+			log.Debugw("Omci_test_Request-ERROR", log.Fields{"id": agent.deviceID, "error": err})
+			return &voltha.TestResponse{Result: voltha.TestResponse_FAILURE}, nil
+		} else {
+			log.Debugw("Omci_test_Request-Success-device-agent", log.Fields{"resp": resp})
+			return resp, nil
+		}
+	}
 }
