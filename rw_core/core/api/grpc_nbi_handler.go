824d649e69f113651e9ed58243f757e0eda20e5f
Dinesh Belwalkar <dumbbel@gmail.com>
2020-02-27 10:41:33 -0800

    [VOL-936] add changes for onu distance from OLT
    Change-Id: If6201799946ac753780d0424c4bd986e2fb394d2

--- rw_core/core/api/grpc_nbi_handler.go
+++ rw_core/core/api/grpc_nbi_handler.go
@@ -808,5 +808,5 @@ func (handler *NBIHandler) StartOmciTestAction(ctx context.Context, omcitestrequ
 
 func (handler *NBIHandler) GetExtValue(ctx context.Context, valueparam *voltha.ValueSpecifier) (*voltha.ReturnValues, error) {
 	log.Debugw("GetValue-request", log.Fields{"onu-id": valueparam.Id})
-	return nil, errors.New("UnImplemented")
+	return handler.deviceMgr.GetExtValue(ctx, valueparam)
 }
