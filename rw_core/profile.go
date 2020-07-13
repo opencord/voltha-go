93b2f62d7bb43165a0e01fa54a25a41edc6f4e2f
Rohan Agrawal <rohan.agrawal01@infosys.com>
2020-07-13 12:07:58 +0000

    [VOL-3187]Add context.Background() in profile.go
    Change-Id: I87e2d1165609972235899376267e67d4c37b33bd

--- rw_core/profile.go
+++ rw_core/profile.go
@@ -19,12 +19,14 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	_ "net/http/pprof"
 )
 
 func realMain() {
 	go func() {
+		ctx := context.Background()
 		logger.Infoln(ctx, http.ListenAndServe("0.0.0.0:6060", nil))
 	}()
 
