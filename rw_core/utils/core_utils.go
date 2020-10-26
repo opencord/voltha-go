7b49dcbbb805ed3ce1147b4d28db857943b22c61
Maninder <maninder@ciena.com>
2020-10-26 11:34:02 +0530

    VOL-3507 Implement the device update queries in rw-core
    Change-Id: I2e9de4935c12981ddb7f10924d629bcd0ec09ef5

--- rw_core/utils/core_utils.go
+++ rw_core/utils/core_utils.go
@@ -34,6 +34,9 @@ func (c contextKey) String() string {
 var (
 	// RPCContextKey for keeping rpc name as metadata
 	rpcContextKey = contextKey("rpc")
+
+	// fromTopicContextKey for keeping entity from which operation is requested as metadata
+	fromTopicContextKey = contextKey("fromTopic")
 )
 
 // ResponseCallback is the function signature for callbacks to execute after a response is received.
@@ -166,6 +169,20 @@ func WithSpanAndRPCMetadataFromContext(sourceCtx context.Context) context.Contex
 	if sourceCtx != nil {
 		targetCtx = log.WithSpanFromContext(targetCtx, sourceCtx)
 		targetCtx = WithRPCMetadataFromContext(targetCtx, sourceCtx)
+	}
+	return targetCtx
+}
+
+func WithFromTopicMetadataContext(ctx context.Context, fromTopic string) context.Context {
+	ctx = context.WithValue(ctx, fromTopicContextKey, fromTopic)
+	return ctx
+}
+
+func WithFromTopicMetadataFromContext(targetCtx, sourceCtx context.Context) context.Context {
+	if sourceCtx != nil {
+		if val, ok := sourceCtx.Value(fromTopicContextKey).(string); ok {
+			targetCtx = context.WithValue(targetCtx, fromTopicContextKey, val)
+		}
 	}
 	return targetCtx
 }
