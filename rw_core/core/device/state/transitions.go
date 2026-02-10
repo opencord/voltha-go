a72dbdeaf643ff05034901067dd706b3fe459aff
balaji.nagarajan <balaji.nagarajan@radisys.com>
2026-02-10 12:41:16 +0530

    [VOL-5570]:Open ONU Adapter Resilency failing for BBSIM
    Change-Id: I5f5cb8c16a12001e4dd74a69b0add53d0b07e8e5
    Signed-off-by: balaji.nagarajan <balaji.nagarajan@radisys.com>

--- rw_core/core/device/state/transitions.go
+++ rw_core/core/device/state/transitions.go
@@ -220,7 +220,7 @@ func NewTransitionMap(dMgr DeviceManager) *TransitionMap {
 			deviceType:    child,
 			previousState: deviceState{Admin: voltha.AdminState_UNKNOWN, Connection: voltha.ConnectStatus_UNKNOWN, Operational: voltha.OperStatus_UNKNOWN, Transient: core.DeviceTransientState_ANY},
 			currentState:  deviceState{Admin: voltha.AdminState_UNKNOWN, Connection: voltha.ConnectStatus_UNKNOWN, Operational: voltha.OperStatus_UNKNOWN, Transient: core.DeviceTransientState_DELETING_POST_ADAPTER_RESPONSE},
-			handlers:      []transitionHandler{dMgr.DeleteAllDeviceFlows, dMgr.ChildDeviceLost, dMgr.DeleteLogicalPorts, dMgr.RunPostDeviceDelete}})
+			handlers:      []transitionHandler{dMgr.DeleteAllDeviceFlows, dMgr.DeleteLogicalPorts, dMgr.ChildDeviceLost, dMgr.RunPostDeviceDelete}})
 	transitionMap.transitions = append(transitionMap.transitions,
 		transition{ // DELETE wait for adapter response(no operation)
 			deviceType:    child,
